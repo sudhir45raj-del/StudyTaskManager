b01ff80b24c1b235cc1892fe57d6aa29d07c08fe
sudhir <sudhir45raj@gmail.com>
2026-09-23 10:45:41 +0530

    filter-to-task

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-function Home(){
-    const [calendars,setCalendars] = useState(new Date())
+function Home() {
+    const [calendars, setCalendars] = useState(new Date())
     const [inputs, setinputs] = useState("")
     const [input, setinput] = useState("")
     const [task, setTask] = useState([])
-    function addTask(){
-        if(inputs.trim() === "" || input.trim() === "") return;
+    const [filter, setFilter] = useState("all");
+    useEffect(()=>{
+       const ts = localStorage.setItem("items",JSON.stringify(task))
+    },[task])
+    // console.log({ts}.getItem("items"))
+    function addTask() {
+        if (inputs.trim() === "" || input.trim() === "") return;
         const newTask = {
-            id:Date.now(),
-            title:inputs,
-            subject:input,
-            duedate:calendars.toDateString(),
+            id: Date.now(),
+            title: inputs,
+            subject: input,
+            duedate: calendars.toDateString(),
             checks: false
         };
-        setTask([...task,newTask])
+        setTask([...task, newTask])
     };
-    function deletetsk(id){
-        setTask(task.filter((item)=> item.id !== id));
+    function deletetsk(id) {
+        setTask(task.filter((item) => item.id !== id));
         console.log("working")
     }
-    function handlecheck(id){
-            setTask(task.map((item)=>{ 
-                    if(item.id === id){
-                        return {...item, checks: !item.checks}
-                    }
-                    return item
-            }))
+    function handlecheck(id) {
+        setTask(task.map((item) => {
+            if (item.id === id) {
+                return { ...item, checks: !item.checks }
+            }
+            return item
+        }))
     }
-    return(
+    const filteredTask =
+        task.filter((item) => {
+            if (filter === "all") {
+                return (
+                    item)
+            }
+            if (filter === "completed") {
+                return (
+                    item.checks === true)
+            }
+            if (filter === "pending") {
+                return (
+                    item.checks === false)
+            }
+        })
+    
+    return (
         <div className="mt-12 mb-5">
-        <h1>Study Task Manager</h1>
+            <h1>Study Task Manager</h1>
             <h2 className="font-bold" >Add New Task</h2>
-        <div> 
-            <input id="ts" className="border-2 pr-10" type="text"placeholder="Write Your Task" onChange={(e)=>setinputs(e.target.value)} ></input>
-            <input id="sb" className=" ml-2" type="text" placeholder="Subject" onChange={(e)=>setinput(e.target.value)} ></input>
-            <span>
-                <h3>Due Date</h3>
-                <Calendar value={calendars} onChange={setCalendars}/>
-                </span>
-            <button className="bg-blue-800 text-white text-sm px-6 " onClick={addTask}>Add Task</button>
             <div>
-                {task.map((item)=>{
-                    return(
-                        <ul key={item.id}>
-                        <li  className="bg-amber-300">
-                            <input type="checkbox" checked={item.checks} onChange={()=>handlecheck(item.id)}></input>
-                                <span className={`bg-green-200 ${item.checks ?  "line-through text-gray-500": ""}`}>
-                                    {item.title} 
-                                    </span>
-                                    <span className={`bg-green-200 ml-5 ${item.checks ? "line-through text-gray-500": ""}`}>
-                                    {item.subject}
-                                    </span>
-                                    <span className= {`bg-green-200 ml-5 ${item.checks ? "line-through text-gray-500": ""}`}>
-                                    {item.duedate}
-                                    </span>
-                                    <button  className="bg-blue-800 ml-5 mb-2 text-white font-bold" onClick={() =>deletetsk(item.id)}>delete</button>
-                        </li>
-                    </ul>
-                    )
-                })}
+                <input id="ts" className="border-2 pr-10" type="text" placeholder="Write Your Task" onChange={(e) => setinputs(e.target.value)} ></input>
+                <input id="sb" className=" ml-2" type="text" placeholder="Subject" onChange={(e) => setinput(e.target.value)} ></input>
+                <span>
+                    <h3>Due Date</h3>
+                    <Calendar value={calendars} onChange={setCalendars} />
+                </span>
+                <button className="bg-blue-800 text-white text-sm px-6 " onClick={addTask}>Add Task</button>
+                <div>
+                    <div className="flex justify-between mt-2 mb-2">
+                        <button className="bg-blue-300 cursor-pointer px-4 rounded-sm" onClick={() => setFilter("all")}>All</button>
+                        <button className="bg-blue-300 cursor-pointer px-4 rounded-sm" onClick={() => setFilter("completed")}>Complete</button>
+                        <button className="bg-blue-300 cursor-pointer px-4 rounded-sm" onClick={() => setFilter("pending")}>Pending</button>
+                    </div>
+                    <div>
+                        {filteredTask.map((item) => {
+                            return (
+                                <ul>
+                                    <li className="bg-red-500">
+                                        <input className="cursor-pointer" type="checkbox" checked={item.checks} onChange={() => handlecheck(item.id)}></input>
+                                        <span className={`bg-green-200 ${item.checks ? "line-through text-gray-500" : ""}`}>
+                                            {item.title}
+                                        </span>
+                                        <span className={`bg-green-200 ml-5 ${item.checks ? "line-through text-gray-500" : ""}`}>
+                                            {item.subject}
+                                        </span>
+                                        <span className={`bg-green-200 ml-5 ${item.checks ? "line-through text-gray-500" : ""}`}>
+                                            {item.duedate}
+                                        </span>
+                                        <button className="bg-blue-800 ml-5 mb-2 text-white font-bold" onClick={() => deletetsk(item.id)}>delete</button>
+                                    </li>
+                                </ul>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
-             </div>
-    </div>
+        </div>
     )
 }
 export default Home;
